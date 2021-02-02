c3b94033ecf8e6bd9448c63658eee0a655b62dbc
rongerwu(吴煌鹏) <rongerwu@tencent.com>
2021-02-02 16:54:51 +0800

    feat(xnet): 添加SDK bugly支持。

--- Classes/xnet/XNet.mm
+++ Classes/xnet/XNet.mm
@@ -16,6 +16,7 @@ __weak static id<Logger> _loggger = nil;
 @implementation XNet
 
 static NSString* host = @"";
+static dispatch_once_t onceToken;
 
 + (int)initWith:(NSString*)appId appKey:(NSString*)appKey appSecretKey:(NSString*)appSecretKey {
     NSLog(@"[TencentXP2P] [qcloud] start init TencentXP2P SDK");
@@ -31,6 +32,23 @@ static NSString* host = @"";
 
     host = [[NSString alloc] initWithCString:(const char*)XP2PService::host().c_str()
                                     encoding:NSASCIIStringEncoding];
+    
+    dispatch_once(&onceToken, ^{
+        NSString * componentId = @"fdd82139ea";
+        NSString * version = [XNet version];
+        if (componentId && version) {
+            NSMutableDictionary * dictionary = [NSMutableDictionary dictionary];
+            // 读取已有信息并记录
+            NSDictionary * dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"BuglySDKInfos"];
+            if (dict) {
+                [dictionary addEntriesFromDictionary:dict];
+            }
+            // 添加当前组件的唯⼀标识和版本
+            [dictionary setValue:version forKey:componentId];
+            // 写⼊更新的信息
+            [[NSUserDefaults standardUserDefaults] setObject:[NSDictionary dictionaryWithDictionary:dictionary] forKey:@"BuglySDKInfos"];
+        }
+    });
     return EXIT_SUCCESS;
 }
 
